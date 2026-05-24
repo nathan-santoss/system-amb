d01908a8fb4a6f232861150963f1a331db1c7365
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-05-24 18:30:36 -0300

    :books: att

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
-import { SyncDB } from "./src/config/syncDB.js";
-SyncDB() // iniciar o servidor sincronizando o banco de dados 
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+import { SyncDB } from './src/config/syncDB.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Inicializa o Express
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// Middleware para JSON
+app.use(express.json())
+
+// Sincroniza banco de dados
+SyncDB()
+
+// Rota teste
+app.get('/', (req, res) => {
+    res.send('API do Sistema de Ambulatório está online!')
+})
+
+// Inicia servidor
+app.listen(PORT, () => {
+    console.log(`==== Servidor Express rodando na porta ${PORT} ====`)
+})
--- src/config/syncDB.js
+++ src/config/syncDB.js
@@ -1,14 +1,16 @@
-import database from "./database.js"
-import Funcionario from './src/models/funcionarios.js'
-import Atendimento from './src/models/atendimento.js'
-import Alergia from './src/models/alergias.js'
-import Atestado from './src/models/atestados.js'
+import database from './database.js'
 
-export async function SyncDB(params) {
+import Funcionario from '../models/funcionarios.js'
+import Atendimento from '../models/atendimento.js'
+import Alergia from '../models/alergias.js'
+import Atestado from '../models/atestados.js'
+
+export async function SyncDB() {
     try {
         await database.sync()
-        console.log('==== BD sincronizado ====');
+
+        console.log('==== BD sincronizado ====')
     } catch (error) {
-        console.error('====== Erro ao sincronizar o banco de dados: ', error, ' ======')
-    }    
+        console.error('==== Erro ao sincronizar o banco de dados ====', error)
+    }
 }
