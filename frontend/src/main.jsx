1cf7997e429f35ffe7e57d98fd93ecc796d577d1
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-05-31 14:46:13 -0300

    :books: att

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-core' 
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
 
