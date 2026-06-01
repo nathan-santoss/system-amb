c68e1b43ce978d3c0820e1a00dbd0ce6e6202402
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-05-31 21:14:27 -0300

    :books: att

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,6 @@ import Login from './pages/Login'
 function App() {
   const [estaLogado, setEstaLogado] = useState(false)
 
-  // 1. A CRIAÇÃO DA FUNÇÃO:
-  // Aqui nós criamos a função de forma clara. 
-  // O que ela faz por dentro? Apenas uma coisa: muda a memória para "true".
   function mudarTelaParaDashboard() {
     setEstaLogado(true)
   }
