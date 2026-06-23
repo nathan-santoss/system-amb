2610c399d5379c19576684fd078fbc295152860b
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-06-23 12:05:19 -0300

    :books: rotas iniciais no react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,16 @@
-import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 
 function App() {
-  const [estaLogado, setEstaLogado] = useState(false)
-
-  function mudarTelaParaDashboard() { // função para informar (true) que o usuário está logado
-    setEstaLogado(true)
-  }
-
-  function renderizarTela() {
-    if (estaLogado === true) {
-      return <Dashboard />
-    } else {
-      return <Login onLoginSuccess={mudarTelaParaDashboard} /> //chamando a função criada
-    }
-  }
-
   return (
-    <div>
-      {renderizarTela()}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+
+         <Route path='/dashboard' element={<Dashboard/>} />
+      </Routes>
+    </BrowserRouter>    
   )
 }
 
