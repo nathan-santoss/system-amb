c4dfefce3db60ac18a7cb2b79e9315827a231f44
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-06-23 18:47:16 -0300

    :books: atualização front-end

--- frontend/src/components/dashboard/ModalFuncionario.jsx
+++ frontend/src/components/dashboard/ModalFuncionario.jsx
@@ -1,4 +1,4 @@
-import '../styles/global.css';
+import '../../styles/global.css';
 
 function ModalFuncionario({ onClose, onSubmit, formValues, onChange }) {
     return (
--- frontend/src/components/dashboard/ModalTriagem.jsx
+++ frontend/src/components/dashboard/ModalTriagem.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../../styles/global.css';
 
 function ModalTriagem({ onClose, onSubmit, formValues, onChange }) {
