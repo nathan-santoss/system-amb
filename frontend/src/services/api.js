dcdf3b124a4653bd1c2a59aae393243c7febce4e
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-06-25 19:25:21 -0300

    :books: att

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 const URL_BASE = 'http://localhost:3000'
 
-export const api = async (endpoint, method = 'GET', data = null) => {
+const api = async (endpoint, method = 'GET', data = null) => {
     const token =  localStorage.getItem('token')
     const headers = {
         'Content-Type': 'application/json'
@@ -18,7 +18,9 @@ export const api = async (endpoint, method = 'GET', data = null) => {
     const response = await fetch(`${URL_BASE}${endpoint}`, config)
     if (response.status === 403 || response.status === 401) {
         console.error("Acesso negado: Token inválido ou expirado.")
-    }
+    }   
 
     return response
 }
+
+export default api
