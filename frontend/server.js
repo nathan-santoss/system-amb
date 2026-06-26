0b3d3cb20e3cd6ae702980d22a9ada9013f55935
Nathan_Santos <nathan.oliver07@hotmail.com>
2026-06-26 10:39:10 -0300

    :books: atualização do front/remove react

---
+++ frontend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 4000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('O servidor EJS está pronto para receber as páginas!'); 
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor de Interface (EJS) a correr na porta ${PORT}`);
+});
